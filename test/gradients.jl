04092226775fa2a4ca86ae8e0e4dd7f7adb33ada
guo chu <guochu604b@gmail.com>
2023-09-24 13:01:40 +0800

    gc changes

--- test/gradients.jl
+++ test/gradients.jl
@@ -1,98 +1,9 @@
 println("------------------------------------")
-println("|         RBM gradients            |")
+println("|          NN gradients            |")
 println("------------------------------------")
 
-# function test_rbm_grad_single(::Type{T}) where T
-# 	n_visible = 2
-# 	nqs = RBM(T, n_hidden=4, n_visible=n_visible)
-# 	state = rand(-1:2:1, n_visible)
 
-# 	loss_origin(nn) = real(Ψ(nn, state))
-
-# 	loss(p) = begin
-# 		reset!(Flux.params(nqs), p)
-# 		return loss_origin(nqs)
-# 	end
-
-# 	grad1 = parameters(gradient(() -> loss_origin(nqs), Flux.params(nqs)))
-# 	grad2 = fdm_gradient(loss, parameters(Flux.params(nqs)))
-
-# 	return maximum(abs.(grad1 - grad2)) < 1.0e-6
-# end
-
-# function test_rbm_grad_batch(::Type{T}) where T
-# 	n_hidden = 30
-# 	n_visible = 5
-# 	n_batch = 100
-# 	nqs = RBM(T, n_hidden=n_hidden, n_visible=n_visible)
-# 	state = rand(-1:2:1, n_visible, n_batch)
-
-# 	loss_origin(nn) = real(sum(Ψ(nn, state)))
-
-# 	loss(p) = begin
-# 		reset!(Flux.params(nqs), p)
-# 		return loss_origin(nqs)
-# 	end
-
-# 	grad1 = parameters(gradient(() -> loss_origin(nqs), Flux.params(nqs)))
-# 	grad2 = fdm_gradient(loss, parameters(Flux.params(nqs)))
-
-# 	return maximum(abs.(grad1 - grad2)) < 1.0e-6
-# end
-
-# function check_local_energy_grad(::Type{T}, L::Int, h::Real, J::Real) where T
-# 	n_visible = L
-# 	nqs = RBM(T, n_hidden=2*n_visible, n_visible=n_visible)
-
-# 	ham = NNQS.IsingChain(h=h, J=J)
-
-# 	loss_origin(nn) = real(energy_exact(ham, nn))
-
-# 	loss(p) = begin
-# 		reset!(Flux.params(nqs), p)
-# 		return loss_origin(nqs)
-# 	end
-
-# 	E, grad1 = energy_and_grad_exact(ham, nqs)
-
-# 	grad2 = fdm_gradient(loss, parameters(Flux.params(nqs)))
-
-# 	# println("energy is $E")
-
-# 	# println(grad1)
-# 	# println(grad2)
-
-# 	return maximum(abs.(grad1 - grad2)) < 1.0e-6
-# 	# return grad1, grad2
-# end
-
-# function check_local_energy_grad_mps(::Type{T}, L::Int, h::Real, J::Real) where T
-# 	nqs = MPS(T, L, D=3)
-
-# 	ham = NNQS.IsingChain(h=h, J=J)
-
-# 	loss_origin(nn) = real(energy_exact(ham, nn))
-
-# 	loss(p) = begin
-# 		reset!(Flux.params(nqs), p)
-# 		return loss_origin(nqs)
-# 	end
-
-# 	E, grad1 = energy_and_grad_exact(ham, nqs)
-
-# 	grad2 = fdm_gradient(loss, parameters(Flux.params(nqs)))
-
-# 	# println("energy is $E")
-
-# 	# println(grad1)
-# 	# println(grad2)
-
-# 	return maximum(abs.(grad1 - grad2)) < 1.0e-6
-# 	# return grad1, grad2
-# end
-
-
-@testset "Checking RBM gradients" begin
+@testset "Checking NN gradients" begin
 	tol = 1.0e-5
 
 	for T in (Float64, ComplexF64)
@@ -155,3 +66,26 @@ println("------------------------------------")
 		end
 	end
 end
+
+@testset "Checking batchautoregressive energy and gradient" begin
+	tol = 1.0e-3
+
+	for T in (Float64, )
+		for L in (4, 5)
+			sampler = BatchAutoRegressiveSampler(L, n_sample_per_chain=10^10)
+			for h in (1, -1)
+				J = 1
+				n_visible = L
+				nqs = MPS(T, n_visible, D=5)
+				ham = NNQS.IsingChain(h=h, J=J)
+				
+				E1, grad1 = energy_and_grad_exact(ham, nqs)
+				E2, grad2 = energy_and_grad(ham, nqs, sampler, verbosity=0)
+
+				@test abs((E1-E2)/ E1) < tol
+				@test norm(grad1 - grad2) / norm(grad1) < tol
+			end
+		end
+	end	
+end
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,7 @@ include("util.jl")
 using Test
 using Random, Zygote
 using NNQS, Flux
+using LinearAlgebra
 
 include("gradients.jl")
 # include("mps.jl")
